9db211b645f072d9dfadf04e487564dd6d7a9da7
RyenMu <muruga13in2022@gmail.com>
2023-02-02 05:29:33 +0530

    basic react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
+
+//parent
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <FirstComponent />
+        <h1>This is my parent Component</h1>
+        <SecondComponent />
+        <FourthComponent/>
+      </div>
+  
   );
+}
+
+class FirstComponent extends React.Component {
+
+  render(){
+
+    return(
+
+      <div className="App">
+        <h1> Mannavan.com</h1>
+      </div>
+
+    )
+  }
+}
+
+class SecondComponent extends React.Component {
+  render(){
+    return(
+      <div>
+      <h2>This is second Component</h2>
+      <ThirdComponent />
+      </div>
+    )
+  }
+}
+
+class ThirdComponent extends React.Component {
+  render(){
+    return(
+      <h2>This is third Component</h2>
+    )
+  }
+}
+
+function FourthComponent(){
+  return(
+    <h2>This is Forth Component - also function based component</h2>
+
+  )
 }
 
 export default App;
